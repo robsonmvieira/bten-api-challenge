6c912aac5648a923ed2d56cdcddcd1d7c398c690
Robson Maia <robsonmvieira@gmail.com>
2021-04-29 00:39:56 -0300

    :recycle: update user interface repository

--- src/infra/repositories/IUserRepository.ts
+++ src/infra/repositories/IUserRepository.ts
@@ -1,9 +1,11 @@
 import { User } from 'modules/users/domain/entities/user.entity'
+import { ICreateUserDTO } from 'modules/users/dtos/create-user.dto'
+import { IUpdateUserDTO } from 'modules/users/dtos/update-user.dto'
 
 export interface IUserRepository {
   getAll(): Promise<User[]>
-  getById(userId: string): Promise<User | undefined>
-  create(user: User): Promise<User>
-  update(userId: string, user: User): Promise<User>
+  getById(userId: string): Promise<User>
+  create(user: ICreateUserDTO): Promise<void>
+  update(userId: string, user: IUpdateUserDTO): Promise<void>
   remove(userId: string): Promise<void>
 }
