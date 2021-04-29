6dfcf8838f623ab0660f69f63821dd9748f0b7e6
Robson Maia <robsonmvieira@gmail.com>
2021-04-29 12:56:49 -0300

    :sparkles: update base class

--- src/modules/users/domain/entities/user.entity.ts
+++ src/modules/users/domain/entities/user.entity.ts
@@ -1,6 +1,14 @@
-export class User {
-  id: string
+import EntityBase from 'infra/database/base-classes/typeorm.entity.base'
+import { Column, Entity } from 'typeorm'
+
+@Entity('users')
+export class User extends EntityBase {
+  @Column({ nullable: false })
   name: string
+
+  @Column({ unique: true })
   email: string
+
+  @Column({ nullable: false })
   password: string
 }
