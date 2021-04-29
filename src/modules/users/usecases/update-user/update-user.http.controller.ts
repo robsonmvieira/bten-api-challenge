33fb9603ac3180042a0774406f7eb87057bd566f
Robson Maia <robsonmvieira@gmail.com>
2021-04-29 00:45:59 -0300

    :sparkles: add update user http controller

---
+++ src/modules/users/usecases/update-user/update-user.http.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { UpdateUserUseCase } from './update-user.usecase'
+
+export class UpdateUserHttpController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const useCase = container.resolve(UpdateUserUseCase)
+    const { id } = req.params
+    const { body } = req
+    await useCase.execute(id, body)
+
+    return res.status(200).send()
+  }
+}
