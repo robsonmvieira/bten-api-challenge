08d7362cfe1ff387a07633a51e59990b80a1ad33
Robson Maia <robsonmvieira@gmail.com>
2021-04-28 23:46:41 -0300

    :sparkles: add Find by id controller

---
+++ src/modules/users/usecases/find-by-id/find-by-id.http.controller.ts
@@ -0,0 +1,13 @@
+import { Response, Request } from 'express'
+import { container } from 'tsyringe'
+
+import { FindByIdUseCase } from './find-by-id.usecase'
+
+export class FindByIdHttpController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const useCase = container.resolve(FindByIdUseCase)
+    const { id } = req.params
+    const user = useCase.execute(id)
+    return res.status(200).json(user)
+  }
+}
