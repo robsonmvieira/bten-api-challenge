f393ac4f189bbfc9f00f1edc1b939abd42742df6
Robson Maia <robsonmvieira@gmail.com>
2021-04-28 23:18:25 -0300

    :recycle: refactor listUserUseCase

--- src/modules/users/usecases/list-users.usecase.ts
+++ src/modules/users/usecases/list-users.usecase.ts
@@ -1,9 +1,15 @@
 import { IUserRepository } from 'infra/repositories/IUserRepository'
 import { inject, injectable } from 'tsyringe'
 
+import { User } from '../domain/entities/user.entity'
+
 @injectable()
 export class ListUsersUseCase {
   constructor(
     @inject('UserRepository') private userRepository: IUserRepository
   ) {}
+
+  execute(): Promise<User[]> {
+    return this.userRepository.getAll()
+  }
 }
