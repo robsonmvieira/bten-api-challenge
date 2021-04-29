1dc4339170475c8ad4fabb5735cf9349649b33b9
Robson Maia <robsonmvieira@gmail.com>
2021-04-29 00:44:21 -0300

    :sparkles: add create user http Controller

---
+++ src/modules/users/usecases/create-user/create-user.http.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { CreateUserUseCase } from './create-user.usecase'
+
+export class CreateUserHttpController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const useCase = container.resolve(CreateUserUseCase)
+    const { name, email, password, confirm_password } = req.body
+    const result = await useCase.execute({
+      name,
+      email,
+      password,
+      confirm_password
+    })
+    return res.status(201).json(result)
+  }
+}
