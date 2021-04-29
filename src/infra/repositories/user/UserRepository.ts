0bfa6c067e9436fec4f143d22fa7b01f45dfb06a
Robson Maia <robsonmvieira@gmail.com>
2021-04-29 00:40:15 -0300

    :recycle: refactor user repository

--- src/infra/repositories/user/UserRepository.ts
+++ src/infra/repositories/user/UserRepository.ts
@@ -1,4 +1,5 @@
 import { User } from 'modules/users/domain/entities/user.entity'
+import { ICreateUserDTO } from 'modules/users/dtos/create-user.dto'
 import { getRepository, Repository } from 'typeorm'
 
 import { IUserRepository } from '../IUserRepository'
@@ -14,14 +15,15 @@ export class UserRepository implements IUserRepository {
   async getById(userId: string): Promise<User> {
     return this.repository.findOne(userId)
   }
-  async create(user: User): Promise<User> {
-    return this.repository.save(user)
+  async create(user: ICreateUserDTO): Promise<void> {
+    await this.repository.save(user)
   }
-  async update(userId: string, user: User): Promise<User> {
-    const result = await this.repository.findOne(userId)
-    const userUpdated = { ...result, ...user }
+  async update(userId: string, user: User): Promise<void> {
+    const userExists = await this.repository.findOne(userId)
+    if (!userExists) {
+      throw new Error('user does not exist')
+    }
     await this.repository.update(userId, user)
-    return userUpdated
   }
   async remove(userId: string): Promise<void> {
     const userExists = await this.getById(userId)
