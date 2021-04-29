2759f1ce0bd6397b4e502068fbbdcfdce7a78914
Robson Maia <robsonmvieira@gmail.com>
2021-04-29 00:43:46 -0300

    :recycle: update user routes

--- src/infra/configs/routes/users/routes.ts
+++ src/infra/configs/routes/users/routes.ts
@@ -1,13 +1,22 @@
 import { Router } from 'express'
+import { CreateUserHttpController } from 'modules/users/usecases/create-user/create-user.http.controller'
+import { FindByIdHttpController } from 'modules/users/usecases/find-by-id/find-by-id.http.controller'
+import { FindUsersHttpController } from 'modules/users/usecases/find-users/find-users.http.controller'
+import { RemoveUserHttpController } from 'modules/users/usecases/remove-user/remove-user.http.controller'
+import { UpdateUserHttpController } from 'modules/users/usecases/update-user/update-user.http.controller'
 
-import { ListUsersHttpController } from '../../../../modules/users/usecases/listUsersHttpController'
+const userRoutes = Router()
 
-const routes = Router()
-const listUserController = new ListUsersHttpController()
-routes.get('/users', listUserController.handle)
-routes.get('/users:id', (req, res) => {})
-routes.post('/users', (req, res) => {})
-routes.put('/users:id', (req, res) => {})
-routes.delete('/users:id', (req, res) => {})
+const findByIdHttpController = new FindByIdHttpController()
+const findUsersHttpController = new FindUsersHttpController()
+const createUserHttpController = new CreateUserHttpController()
+const updateUserHttpController = new UpdateUserHttpController()
+const removeUserHttpController = new RemoveUserHttpController()
 
-export default routes
+userRoutes.get('/', findUsersHttpController.handle)
+userRoutes.get('/:id', findByIdHttpController.handle)
+userRoutes.post('/', createUserHttpController.handle)
+userRoutes.put('/:id', updateUserHttpController.handle)
+userRoutes.delete('/:id', removeUserHttpController.handle)
+
+export default userRoutes
