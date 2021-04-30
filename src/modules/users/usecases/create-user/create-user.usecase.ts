1545e7902a8862cd8b9255f3c47857870850c60a
Robson Maia <robsonmvieira@gmail.com>
2021-04-30 16:57:32 -0300

    :recycle: improve business logic to create new user

--- src/modules/users/usecases/create-user/create-user.usecase.ts
+++ src/modules/users/usecases/create-user/create-user.usecase.ts
@@ -1,5 +1,7 @@
 import { inject, injectable } from 'tsyringe'
 
+import { ArgumentInvalidException } from '../../../../core/exceptions/argument-invalid.exception'
+import { ConflictException } from '../../../../core/exceptions/conflict.exception'
 import { IHash } from '../../../../core/interface-adapters/interfaces/hash.adapter'
 import { IUserRepository } from '../../../../infra/repositories/IUserRepository'
 import { CreateUserRequest } from './create-user.dto.request'
@@ -18,8 +20,15 @@ export class CreateUserUseCase {
     confirm_password
   }: CreateUserRequest): Promise<void> {
     if (password !== confirm_password) {
-      throw new Error('Password does not match')
+      throw new ArgumentInvalidException('Password does not match', 400)
     }
+
+    const emailAlreadyExists = await this.userRepository.findByEmail(email)
+
+    if (emailAlreadyExists) {
+      throw new ConflictException('Email already Exists', 400)
+    }
+
     const passwordHashed = await this.hashAdapter.hash(password, 8)
 
     this.userRepository.create({ name, password: passwordHashed, email })
