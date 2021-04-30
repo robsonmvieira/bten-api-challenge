9ed1fcc9afc4b4ef3a1cb2ab2de49569dedd1ba4
Robson Maia <robsonmvieira@gmail.com>
2021-04-30 00:37:30 -0300

    :sparkles: update DI container

--- src/infra/configs/dependency-injection/index.ts
+++ src/infra/configs/dependency-injection/index.ts
@@ -1,6 +1,9 @@
 import { container } from 'tsyringe'
 
+import { IHash } from '../../../core/interface-adapters/interfaces/hash.adapter'
+import { HashAdapter } from '../../adapters/hash/jwt.hash.adapter'
 import { IUserRepository } from '../../repositories/IUserRepository'
 import { UserRepository } from '../../repositories/user/UserRepository'
 
-container.registerSingleton<IUserRepository>('UserRepository', UserRepository)
+container.registerSingleton<IUserRepository>('IUserRepository', UserRepository)
+container.registerSingleton<IHash>('IHashAdapter', HashAdapter)
