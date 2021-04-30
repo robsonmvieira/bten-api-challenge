c2ab5749b7ec7c9295ce696a4e1ac6fefec98ab0
Robson Maia <robsonmvieira@gmail.com>
2021-04-30 00:34:53 -0300

    :recycle: refactor createuser usecase

--- src/modules/users/usecases/create-user/create-user.usecase.ts
+++ src/modules/users/usecases/create-user/create-user.usecase.ts
@@ -1,15 +1,27 @@
-import { ICreateUserDTO } from 'modules/users/dtos/create-user.dto'
 import { inject, injectable } from 'tsyringe'
 
+import { IHash } from '../../../../core/interface-adapters/interfaces/hash.adapter'
 import { IUserRepository } from '../../../../infra/repositories/IUserRepository'
+import { CreateUserRequest } from './create-user.dto.request'
 
 @injectable()
 export class CreateUserUseCase {
   constructor(
-    @inject('IUserRepository') private userRepository: IUserRepository
+    @inject('IUserRepository') private userRepository: IUserRepository,
+    @inject('IHashAdapter') private hashAdapter: IHash
   ) {}
 
-  async execute(createUserDto: ICreateUserDTO): Promise<void> {
-    this.userRepository.create(createUserDto)
+  async execute({
+    name,
+    password,
+    email,
+    confirm_password
+  }: CreateUserRequest): Promise<void> {
+    if (password !== confirm_password) {
+      throw new Error('Password does not match')
+    }
+    const passwordHashed = await this.hashAdapter.hash(password, 8)
+
+    this.userRepository.create({ name, password: passwordHashed, email })
   }
 }
