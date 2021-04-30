fedd1c03097e4308f51d1c49309f901ca91dc837
Robson Maia <robsonmvieira@gmail.com>
2021-04-30 16:53:22 -0300

    :goal_net: add error middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import { config } from 'dotenv'
 import express from 'express'
+import 'express-async-errors'
 import './infra/database'
 
 import 'reflect-metadata'
 import './infra/configs/dependency-injection'
+
 import routes from './infra/configs/routes'
+import errorInterceptor from './infra/interceptors/error.interceptor'
 
 config()
 
@@ -12,6 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
+app.use(errorInterceptor)
 app.listen(process.env.PORT || 8000, () =>
   console.log('app running at port 8000')
 )
