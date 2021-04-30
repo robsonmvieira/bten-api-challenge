cf85c9be288d54d1f38be96a390199a021d9e59d
Robson Maia <robsonmvieira@gmail.com>
2021-04-30 16:54:18 -0300

    :sparkles: add session routes

--- src/infra/configs/routes/index.ts
+++ src/infra/configs/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 
-import userRoutes from './users/routes'
+import sessionsRoutes from './sessions/sessions.routes'
+import userRoutes from './users/users.routes'
 
 const routes = Router()
 routes.use('/api/users', userRoutes)
+routes.use(sessionsRoutes)
 
 export default routes
