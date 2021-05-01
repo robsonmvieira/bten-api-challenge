f75c001bc711fe6aafa02f6cec0dcda417bd84f2
Robson Maia <robsonmvieira@gmail.com>
2021-05-01 20:13:02 -0300

    :white_check_mark: add repository test

---
+++ src/modules/users/database/user.in-memory.repository.ts
@@ -0,0 +1,46 @@
+import { NotFoundException } from '../../../core/exceptions/not-found.exception'
+import { IUserRepository } from '../../../infra/repositories/user/IUserRepository'
+import { User } from '../domain/entities/user.entity'
+import { ICreateUserDTO } from '../dtos/create-user.dto'
+import { IUpdateUserDTO } from '../dtos/update-user.dto'
+
+export class UserInMemoryRepository implements IUserRepository {
+  users: User[] = []
+  async getAll(): Promise<User[]> {
+    return this.users
+  }
+  async getById(userId: string): Promise<User> {
+    const user = this.users.find(user => user.id === userId)
+
+    if (!user) {
+      throw new NotFoundException('user not found', 404)
+    }
+    return user
+  }
+  async create(user: ICreateUserDTO): Promise<void> {
+    const newUser = new User()
+    newUser.id = String(this.users.length + 1)
+    Object.assign(newUser, user)
+    this.users.push(newUser)
+  }
+  async update(userId: string, user: IUpdateUserDTO): Promise<void> {
+    const userExists = this.users.find(user => user.id === userId)
+    if (!userExists) {
+      throw new NotFoundException('User not found', 404)
+    }
+    Object.assign(userExists, user)
+    this.users = this.users.map(el => (el.id !== userId ? el : userExists))
+  }
+  async remove(userId: string): Promise<void> {
+    const user = this.users.find(user => user.id === userId)
+
+    if (!user) {
+      throw new NotFoundException('User not found', 404)
+    }
+    this.users = this.users.filter(user => user.id !== userId)
+  }
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find(user => user.email === email)
+    return user
+  }
+}
