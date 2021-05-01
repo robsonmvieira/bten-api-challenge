e67a9ff87db1988a6345d8b7c1344f1e5a3966a6
Robson Maia <robsonmvieira@gmail.com>
2021-04-30 23:33:30 -0300

    :recycle: refactor user repository methods

--- src/infra/repositories/user/UserRepository.ts
+++ src/infra/repositories/user/UserRepository.ts
@@ -1,7 +1,9 @@
-import { ICreateUserDTO } from 'modules/users/dtos/create-user.dto'
 import { getRepository, Repository } from 'typeorm'
 
+import { NotFoundException } from '../../../core/exceptions/not-found.exception'
 import { User } from '../../../modules/users/domain/entities/user.entity'
+import { ICreateUserDTO } from '../../../modules/users/dtos/create-user.dto'
+import { IUpdateUserDTO } from '../../../modules/users/dtos/update-user.dto'
 import { IUserRepository } from '../IUserRepository'
 
 export class UserRepository implements IUserRepository {
@@ -16,24 +18,30 @@ export class UserRepository implements IUserRepository {
     return this.repository.find()
   }
   async getById(userId: string): Promise<User> {
-    return this.repository.findOne(userId)
+    const user = await this.repository.findOne(userId)
+
+    if (!user) {
+      throw new NotFoundException('User not found', 404)
+    }
+    return user
   }
   async create(user: ICreateUserDTO): Promise<void> {
     await this.repository.save(user)
   }
-  async update(userId: string, user: User): Promise<void> {
+  async update(userId: string, user: IUpdateUserDTO): Promise<void> {
     const userExists = await this.repository.findOne(userId)
+
     if (!userExists) {
-      throw new Error('user does not exist')
+      throw new NotFoundException('User not found', 404)
     }
     await this.repository.update(userId, user)
   }
   async remove(userId: string): Promise<void> {
-    const userExists = await this.getById(userId)
+    const user = await this.repository.findOne(userId)
 
-    if (!userExists) {
-      throw new Error("User Doesn't exists")
+    if (!user) {
+      throw new NotFoundException('User not found', 404)
     }
-    await this.repository.delete(userExists.id)
+    await this.repository.delete(user.id)
   }
 }
